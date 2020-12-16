0abc2479da871d681620743cd8de9a66c898b71a
Jordi Keijzers <jordi.keijzers@rocdeleijgraaf.nl>
2020-12-16 14:45:50 +0100

    Rekensommen voor groep 5 en 6 toegevoegd.

--- rekenen/groep6.php
+++ rekenen/groep6.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 
+<?php
+    $tafel_van = 7;
+?>
+
 <html>
     <head>
         <link rel="stylesheet" href="../css/header.css">
         <link rel="stylesheet" href="../css/footer.css">
         <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/rekenen.css">
 
         <title>De Splinter - Rekenen - Groep 6</title>
     </head>
@@ -24,6 +29,30 @@
             <section>
                 <h2>Rekensommen voor groep 6</h2>
                 <a href="../reken.html">Terug naar groep selectie</a>
+                <form action="groep6.php" method="POST" id="som-input">
+                    <div id="tafels">
+                        <h2>Tafels</h2>
+                        <h3>Tafel van <?php echo $tafel_van; ?></h3>
+                        <?php
+                            for ($getal = 1; $getal < 11; $getal++) {
+                                $gegeven_antwoord = "";
+                                $is_antwoord_correct = true;
+
+                                if (isset($_POST["tafels_som$getal"])) {
+                                    $gegeven_antwoord = $_POST["tafels_som$getal"];
+                                    $correct_antwoord = intval($getal) * $tafel_van;
+                                    $is_antwoord_correct = intval($gegeven_antwoord) == $correct_antwoord;
+                                }
+
+                                echo "<div class=\"som\">";
+                                echo "<label>$getal x $tafel_van =</label>";
+                                echo "<input type=\"text\" value=\"$gegeven_antwoord\" " . ($is_antwoord_correct ? "" : " class=\"incorrect\"") . "placeholder=\"Typ jouw antwoord hier...\" name=\"tafels_som$getal\">";
+                                echo "</div>";
+                            }
+                        ?>
+                    </div>
+                    <input type="submit" value="Controleren">
+                </form>
             </section>
         </div>
         <footer>
