0ce2bceda9f242ff897fc8b795f577a3611bb35f
Jordi Keijzers <jordi.keijzers@rocdeleijgraaf.nl>
2021-01-06 11:36:57 +0100

    Cijfers toegevoegd voor toetsen groep 5

--- toetsen/groep5/plus-sommen.php
+++ toetsen/groep5/plus-sommen.php
@@ -37,7 +37,12 @@
                         <h2>Plus sommen</h2>
                         <?php
                             function maak_tafel(int $plus_getal) {
+                                echo "<section class=\"vragen\">";
                                 echo "<h3>Plus sommen met $plus_getal</h3>";
+
+                                $totale_antwoorden = 0;
+                                $antwoorden_goed = 0;
+                                $antwoord_ingevuld = false;
 
                                 for ($getal = 1; $getal < 11; $getal++) {
                                     $gegeven_antwoord = "";
@@ -47,18 +52,55 @@
                                         $gegeven_antwoord = $_POST["som$plus_getal$getal"];
                                         $correct_antwoord = intval($getal) + $plus_getal;
                                         $is_antwoord_correct = intval($gegeven_antwoord) == $correct_antwoord;
+
+                                        $antwoord_ingevuld = true;
+
+                                        if ($is_antwoord_correct) {
+                                            $antwoorden_goed++;
+                                        }
                                     }
+
+                                    $totale_antwoorden++;
 
                                     echo "<div class=\"som\">";
                                     echo "<label>$getal + $plus_getal =</label>";
                                     echo "<input type=\"text\" value=\"$gegeven_antwoord\" " . ($is_antwoord_correct ? "" : " class=\"incorrect\"") . "placeholder=\"Typ jouw antwoord hier...\" name=\"som$plus_getal$getal\">";
                                     echo "</div>";
                                 }
+
+                                if ($antwoord_ingevuld) {
+                                    $cijfer = $antwoorden_goed / $totale_antwoorden * 9 + 1;
+
+                                    echo "<p><strong>Je hebt $antwoorden_goed van de $totale_antwoorden antwoorden goed. Jouw cijfer voor dit onderdeel is dus een $cijfer.</strong></p>";
+                                    echo "</section>";
+
+                                    return $cijfer;
+                                }
+
+                                echo "</section>";
                             }
 
-                            maak_tafel($getal_1);
-                            maak_tafel($getal_2);
-                            maak_tafel($getal_3);
+                            $cijfers = array();
+
+                            $cijfers[] = maak_tafel($getal_1);
+                            $cijfers[] = maak_tafel($getal_2);
+                            $cijfers[] = maak_tafel($getal_3);
+
+                            $cijfer = 0;
+                            $aantal_cijfers = 0;
+
+                            foreach ($cijfers as $_ => $onderdeel_cijfer) {
+                                $cijfer += $onderdeel_cijfer;
+                                $aantal_cijfers++;
+                            }
+
+                            if ($aantal_cijfers > 0) {
+                                $cijfer = $cijfer / $aantal_cijfers;
+
+                                $color = $cijfer >= 5.5 ? "#00aa00" : "#aa0000";
+
+                                echo "<p style=\"color: $color\"><strong>Jouw punt voor deze toets is een $cijfer</strong></p>";
+                            }
                         ?>
                     </div>
                     <input type="submit" value="Controleren">
--- toetsen/groep5/vermenigvuldigen.php
+++ toetsen/groep5/vermenigvuldigen.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 
 <?php
-    $tafels_1 = 4;
-    $tafels_2 = 2;
-    $tafels_3 = 5;
+    $tafels_1 = 5;
+    $tafels_2 = 4;
+    $tafels_3 = 8;
 ?>
 
 <html>
@@ -37,7 +37,12 @@
                         <h2>Tafels</h2>
                         <?php
                             function maak_tafel(int $tafel) {
+                                echo "<section class=\"vragen\">";
                                 echo "<h3>Tafel van $tafel</h3>";
+
+                                $totale_antwoorden = 0;
+                                $antwoorden_goed = 0;
+                                $antwoord_ingevuld = false;
 
                                 for ($getal = 1; $getal < 11; $getal++) {
                                     $gegeven_antwoord = "";
@@ -49,16 +54,51 @@
                                         $is_antwoord_correct = intval($gegeven_antwoord) == $correct_antwoord;
                                     }
 
+                                    $antwoord_ingevuld = true;
+
+                                    if ($is_antwoord_correct) {
+                                        $antwoorden_goed++;
+                                    }
+
                                     echo "<div class=\"som\">";
                                     echo "<label>$getal x $tafel =</label>";
                                     echo "<input type=\"text\" value=\"$gegeven_antwoord\" " . ($is_antwoord_correct ? "" : " class=\"incorrect\"") . "placeholder=\"Typ jouw antwoord hier...\" name=\"som$tafel$getal\">";
                                     echo "</div>";
                                 }
+
+                                if ($antwoord_ingevuld) {
+                                    $cijfer = $antwoorden_goed / $totale_antwoorden * 9 + 1;
+
+                                    echo "<p><strong>Je hebt $antwoorden_goed van de $totale_antwoorden antwoorden goed. Jouw cijfer voor dit onderdeel is dus een $cijfer.</strong></p>";
+                                    echo "</section>";
+
+                                    return $cijfer;
+                                }
+
+                                echo "</section>";
                             }
 
-                            maak_tafel($tafels_1);
-                            maak_tafel($tafels_2);
-                            maak_tafel($tafels_3);
+                            $cijfers = array();
+
+                            $cijfers[] = maak_tafel($getal_1);
+                            $cijfers[] = maak_tafel($getal_2);
+                            $cijfers[] = maak_tafel($getal_3);
+
+                            $cijfer = 0;
+                            $aantal_cijfers = 0;
+
+                            foreach ($cijfers as $_ => $onderdeel_cijfer) {
+                                $cijfer += $onderdeel_cijfer;
+                                $aantal_cijfers++;
+                            }
+
+                            if ($aantal_cijfers > 0) {
+                                $cijfer = $cijfer / $aantal_cijfers;
+
+                                $color = $cijfer >= 5.5 ? "#00aa00" : "#aa0000";
+
+                                echo "<p style=\"color: $color\"><strong>Jouw punt voor deze toets is een $cijfer</strong></p>";
+                            }
                         ?>
                     </div>
                     <input type="submit" value="Controleren">
